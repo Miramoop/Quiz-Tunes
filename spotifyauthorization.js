7b7461f3420a73fa2c8b4eb84735467d729f9054
Miranda Morris <mmorris@bizstream.com>
2024-05-23 09:58:50 -0400

    Current State

---
+++ spotifyauthorization.js
@@ -0,0 +1,16 @@
+// const clientId = "";
+// const redirectUri = "http://localhost:8080";
+// const authorizationEndpoint = "https://accounts.spotify.com/authorize";
+
+
+//Authorization Code with PKCE Flow:
+
+//Code Challenge using a Code Verifier
+
+//Returns the base64 representation of the digest
+
+//Request Authorization from the user and retrieve the authorization code
+
+//Request an access token from the authorization code
+
+//Use the access token to make API Calls
